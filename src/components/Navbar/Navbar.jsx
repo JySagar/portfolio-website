947251dc328c4f0cf748d1c6c3b871a658b5a887
JySagar <sagardax101@gmail.com>
2025-08-30 03:41:37 +0530

    its good night and next morning analyze lines 112 to 135

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -108,6 +108,31 @@ const Navbar = () => {
             />
           )}
         </div>
+        {/* Mobile Menu Items */}
+        <div>
+          {isOpen && (
+            <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg">
+              <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+                {menuItems.map((item) => (
+                  <li
+                    key={item.id}
+                    className={`curson-pointer hover:text-white ${
+                      activeSection === item.id ? "text-[#8245ec]" : ""
+                    }`}
+                  >
+                    <button
+                      onClick={() => {
+                        handleMenuItemClick(item.id);
+                      }}
+                    >
+                      {item.label}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
