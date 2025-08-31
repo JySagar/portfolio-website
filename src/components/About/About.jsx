c964f0e2528187217d684072949a7e8692b62163
JySagar <sagardax101@gmail.com>
2025-09-01 02:32:49 +0530

    adding about section where divided the div into 1/2 i.e. half and then making sections for each item like h1 h2 etc then now will add skills heading with typing effect

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side  */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+            {/* Name */}
+            <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+              Sagarjyoti Das
+            </h2>
+            {/* Skills heading with typing effect */}
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
