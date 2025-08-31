3b2adb3e2feb4805fb750ad67f7473a067f0d1e3
JySagar <sagardax101@gmail.com>
2025-09-01 02:51:09 +0530

    one problem: react typing effect is outdated i.e. for 16/17 now its react 18/19 so lets use another i.e. react type animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTypingEffect from "react-typing-effect";
 
 const About = () => {
   return (
@@ -12,12 +13,30 @@ const About = () => {
           {/* Greeting */}
           <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight">
             Hi, I am
-            {/* Name */}
-            <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
-              Sagarjyoti Das
-            </h2>
-            {/* Skills heading with typing effect */}
           </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Sagarjyoti Das
+          </h2>
+          {/* Skills heading with typing effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                "Fullstack Developer",
+                "App Developer",
+                "UI/UX Developer",
+                "Coder",
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
         </div>
       </div>
     </section>
