2b162c093323ec795296c5dc7ab728fa464830e8
JySagar <sagardax101@gmail.com>
2025-09-01 04:20:53 +0530

    Added Resume Button to download CV just the link to href is remaining i.e. from G Drive of sagardax101

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,6 +46,19 @@ const About = () => {
             modern technologies to create seamless user experiences and
             efficient soulutions.
           </p>
+          {/* Resume Button */}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
         </div>
       </div>
     </section>
