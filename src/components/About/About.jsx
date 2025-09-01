4f338966741ce13e22b0b355bf3e8f71c6846758
JySagar <sagardax101@gmail.com>
2025-09-02 01:09:25 +0530

    DOWNLOAD CV done now will do Right Side

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href=""
+            href="https://drive.google.com/file/d/1EIVQ9Wo5yo6gNsVB7VkGEC96d-ZRWaYg/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -60,6 +60,7 @@ const About = () => {
             DOWNLOAD CV
           </a>
         </div>
+        {/* Right Side */}
       </div>
     </section>
   );
