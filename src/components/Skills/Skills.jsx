79d111b45d8202d4e17dc68aeff129cba4047768
JySagar <sagardax101@gmail.com>
2025-09-02 01:41:50 +0530

    made linear gradient on tailwind congif js and given on Skills section classname and also imported SkillsInfor from constants.js

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,13 @@
+import { SkillsInfo } from "../../constants";
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section
+      id="skills"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient"
+    ></section>
+  );
 };
 
 export default Skills;
