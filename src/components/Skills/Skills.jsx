08d2210c38af57a114e0e9a0020a72dcb4ae110c
JySagar <sagardax101@gmail.com>
2025-09-07 21:02:39 +0530

    we added md:hidden so that the menu when opened in small screen then when maximized then it disappears

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -24,8 +24,26 @@ const Skills = () => {
             className="bg-gray-900 backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
           >
             <h3 className="text-2xl sm:text-3xl font-semibold text-gray-400 mb-4 text-center">
-              {/* {category.title} */}
+              {category.title}
             </h3>
+            {/* Skill Items - 3 per row on larger screens */}
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 w-full">
+              {category.skills.map((skill) => {
+                <div
+                  key={skill.name}
+                  className="flex items-center justify-center space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-2"
+                >
+                  <img
+                    src={skill.logo}
+                    alt={`${skill.name} logo`}
+                    className="w-6 h-6 sm:w-8 sm:h-8"
+                  />
+                  <span className="text-xs sm:text-sm text-gray-300">
+                    {skill.name}
+                  </span>
+                </div>;
+              })}
+            </div>
           </div>
         ))}
       </div>
