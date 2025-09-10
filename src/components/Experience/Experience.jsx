163aba823c8f39e52ada508f1799f3b2b4d65101
JySagar <sagardax101@gmail.com>
2025-09-10 16:00:07 +0530

    editing experiences

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { experiences } from "../../constants";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section
+      id="experience"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient"
+    >
+      {/* Section Title */}
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">EXPERIENCE</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A collection of my work experience and the roles I have taken in
+          various organizations
+        </p>
+      </div>
+
+      {/* Experience Timeline */}
+      <div className="relative">
+        {/* Vertical Line */}
+        <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full">
+          {/* Experience entries */}
+          {experiences.map((experience, index) => {
+            <div
+              key={experience.id}
+              className={`flex flex-col sm:flex-row items-center mb-16. ${
+                index % 2 === 0 ? "sm:justify-end" : "sm:justify-start"
+              }`}
+            ></div>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
