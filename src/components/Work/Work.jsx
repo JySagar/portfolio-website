0897d58e1de93b8d0cad2eb3fdc4dc9476f508e5
Sagar <sagardax101@gmail.com>
2025-10-09 22:14:12 +0530

    work sectiond one

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -30,9 +30,7 @@ const Work = () => {
         {projects.map((project) => (
           <div
             key={project.id}
-            onClick={() => {
-              handleOpenModal(project);
-            }}
+            onClick={() => handleOpenModal(project)}
             className="border border-white bg-gray-900 backdrop-blur-md rounded-2xl shadow-2xl overflow-hidden cursor-pointer hover:shadow-purple-500/50 hover:-translate-y-2 transition-transform duration-300"
           >
             <div className="p-4">
@@ -57,7 +55,7 @@ const Work = () => {
               </div>
               <div className="flex gap-4">
                 <a
-                  href={selectedProject.github}
+                  href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="w-1/2 bg-"
@@ -101,11 +99,29 @@ const Work = () => {
                   {selectedProject.tags.map((tag, index) => (
                     <span
                       key={index}
-                      className="inline bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1 mr-2 mb-2"
+                      className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
                     >
                       {tag}
                     </span>
                   ))}
+                </div>
+                <div className="flex gap-4">
+                  <a
+                    href={selectedProject.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-1/2 bg-gray-800 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Code
+                  </a>
+                  <a
+                    href={selectedProject.webapp}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-1/2 bg-purple-600 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Live
+                  </a>
                 </div>
               </div>
             </div>
